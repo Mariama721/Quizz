d9dd2164ddd8eac9342861b4ca866553d635d466
Mariama721 <mariamabah8@outlook.fr>
2026-03-12 11:22:47 +0100

    premier commit

---
+++ adaquiz/src/accueil.js
@@ -0,0 +1,4 @@
+const bouton = document.createElement("button");
+bouton.textContent = "Démarrer";
+document.body.appendChild(bouton);
+l,ok,
---
+++ adaquiz/src/main.js
@@ -0,0 +1,5 @@
+import "./style.css";
+import quiz from "./quiz-femmes-scientifiques.json";
+import "./accueil";
+
+document.querySelector("#app > h1").innerText = quiz.title;
